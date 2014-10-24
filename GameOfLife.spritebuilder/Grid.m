9be78075fd410a4dbbe9e82ec9c26e40c8f326fc
Jason Ash <jasonwadeash@gmail.com>
2014-10-23 23:30:45 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -153,9 +153,7 @@ static const int GRID_COLUMNS = 10;
 		{
 				// access the creature in the cell that corresponds to the current row/column
 			Creature *currentCreature = _gridArray[i][j];
-	  
-				// remember that every creature has a 'livingNeighbors' property that we created earlier
-			currentCreature.livingNeighbors = 0;
+			
 			if (currentCreature.livingNeighbors == 3) {
 				currentCreature.isAlive = TRUE;
 				numAlive++;
